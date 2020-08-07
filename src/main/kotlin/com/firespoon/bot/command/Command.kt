e165693c71a876e37a7a4bc854685cc6365e9727
FireSpoon <460401633@qq.com>
2020-08-07 21:45:35 +0800

    spoon v0.0.1

--- src/main/kotlin/com/firespoon/bot/command/Command.kt
+++ src/main/kotlin/com/firespoon/bot/command/Command.kt
@@ -5,28 +5,23 @@ import net.mamoe.mirai.message.MessageEvent
 
 open class Command<E : MessageEvent>
     (
-    regex: Regex,
-    val action: suspend CommandBody<E>.() -> Unit,
-    val builder: suspend (E) -> CommandBody<E>? = { event ->
-        CommandAnalyzer.analyze(event, regex)
-    }
+    var builder: suspend (E) -> CommandBody<E>?,
+    val action: suspend CommandBody<E>.() -> Unit
 ) {
+
+    constructor (
+        regex: Regex,
+        action: suspend CommandBody<E>.() -> Unit
+    ) : this({ event ->
+        CommandAnalyzer.analyze(event, regex)
+    }, action)
+
     constructor(
         keyword: String,
         action: suspend CommandBody<E>.() -> Unit
     ) : this(
-        //  \s* .{keyword} (\s*[^\s]+)* \s*
+        //  pattern = \s*.{keyword}(\s*[^\s]+)* \s*
         Regex("(?:\\s*\\.${keyword})((\\s*[^\\s]+)*)(?:\\s*)")
         , action
-    )
-
-    constructor(
-        keyword: String,
-        action: suspend CommandBody<E>.() -> Unit,
-        builder: suspend (E) -> CommandBody<E>?
-    ) : this(
-        //  \s* .{keyword} (\s*[^\s]+)* \s*
-        Regex("(?:\\s*\\.${keyword})((\\s*[^\\s]+)*)(?:\\s*)")
-        , action, builder
     )
 }
