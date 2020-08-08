b102335bd90e6b7c2985c97bd6a131f8ac8867b6
“Aye10032 <yeyu6_505@126.com>
2020-08-08 22:01:03 +0800

    add device info

--- src/main/kotlin/com/aye10032/MyLoader.kt
+++ src/main/kotlin/com/aye10032/MyLoader.kt
@@ -1,5 +1,7 @@
 package com.aye10032
 
+import com.aye10032.command.NMSLCommand
+import com.firespoon.bot.command.DiceCommand
 import com.firespoon.bot.core.boot
 import com.firespoon.bot.core.registerCommandAlways
 import net.mamoe.mirai.Bot
@@ -9,8 +11,12 @@ suspend fun main() {
     val qqId = 1969631968L
     val password = "123456789yy"
 
-    val bot = Bot(qqId, password)
+    val bot = Bot(qqId, password){
+        fileBasedDeviceInfo("device.json")
+    }
     bot.boot()
+
+    bot.registerCommandAlways(NMSLCommand.command)
 
     bot.join()
 }
