db46574b81a94badd653ba669b7a72db7fafbb3b
FireSpoon <460401633@qq.com>
2020-08-09 11:01:08 +0800

    fsbot 0.0.3

--- src/main/kotlin/com/firespoon/bot/command/CommandAnalyzer.kt
+++ src/main/kotlin/com/firespoon/bot/command/CommandAnalyzer.kt
@@ -6,12 +6,42 @@ import net.mamoe.mirai.message.data.content
 
 abstract class CommandAnalyzer {
     companion object {
-        fun <E : MessageEvent>
-                analyze(
+        /**
+         * 将一个event根据正则表达式解析成一个CommandBody
+         *
+         * CommandBody.args为regex解析后的每一个group，但是不包括group[0]
+         */
+        fun <E : MessageEvent> analyze(
             event: E,
             regex: Regex
         ): CommandBody<E>? {
             val commandString = event.message.content
+
+            val matchResult = regex.matchEntire(commandString)
+                ?: return null
+
+            val groups = matchResult.groupValues
+            val args = Array<Any>(groups.size - 1) {it ->
+                matchResult.groupValues[it + 1]
+            }
+
+            return CommandBody<E>(event, args)
+        }
+
+        /**
+         * 将一个event根据keyword解析成一个CommandBody
+         *
+         * 命令格式为 .${keyword} arg0 arg1 ...
+         *
+         * CommandBody.args = {arg0 arg1 ...}
+         */
+        fun <E : MessageEvent> analyze(
+            event: E,
+            keyword: String
+        ): CommandBody<E>? {
+            val commandString = event.message.content
+
+            val regex = Regex("(?:\\s*\\.${keyword})((\\s*[^\\s]+)*)(?:\\s*)")
 
             val matchResult = regex.matchEntire(commandString)
                 ?: return null
---
+++ src/main/kotlin/com/firespoon/command/DiceCommand.kt
@@ -0,0 +1,112 @@
+package com.firespoon.command
+
+import com.firespoon.bot.command.Command
+import net.mamoe.mirai.message.GroupMessageEvent
+import net.mamoe.mirai.message.MessageEvent
+import net.mamoe.mirai.message.data.At
+import net.mamoe.mirai.message.data.Message
+import net.mamoe.mirai.message.data.asMessageChain
+import net.mamoe.mirai.message.data.toMessage
+import java.lang.Exception
+import java.util.*
+
+abstract class DiceCommand {
+    companion object {
+        val command = Command<MessageEvent>(
+            name = "Dice",
+            keyword = "r",
+            action = {
+                if (args.isEmpty()) {
+                    val res = (0..100).random()
+                    reply(res.toString())
+                } else {
+                    val replyMessage = LinkedList<Message>()
+                    if (event is GroupMessageEvent) {
+                        replyMessage.add(At(event.sender))
+                    }
+
+
+                    val command = "+${args[0]}"
+
+                    val ops = listOf("+", "-", "*", "/")
+
+                    var i = command.indexOfAny(ops)
+                    var sum = 0L
+                    while (i != -1) {
+                        val j = command.indexOfAny(ops, i + 1)
+                        val subCommand = if (j == -1) {
+                            command.substring(i + 1)
+                        } else {
+                            command.substring(i + 1, j)
+                        }
+                        val temp = analyze(subCommand)
+                        val time = temp.first
+                        val max = temp.second
+
+                        if (time == -1) {
+                            replyMessage.add("您的骰点结果已被淹没在骰子的海洋中".toMessage())
+                            sum = 0
+                            break
+                        } else if (time == -2) {
+                            replyMessage.add("正在尝试从十二维空间中寻找面数足够多的骰子".toMessage())
+                            sum = 0
+                            break
+                        } else {
+                            val res = when (temp.first) {
+                                0 -> temp.second.toLong()
+                                else -> randomOnce(
+                                    time,
+                                    max
+                                ).sum()
+                            }
+
+                            when (command[i]) {
+                                '+' -> sum += res
+                                '-' -> sum -= res
+                                '*' -> sum *= res
+                                '/' -> sum /= res
+                            }
+                        }
+
+                        i = j
+                    }
+
+                    if (sum > 0) {
+                        replyMessage.add("您的骰点结果为： $sum".toMessage())
+                    }
+                    reply(replyMessage.asMessageChain())
+                }
+            }
+        )
+
+        private fun analyze(command: String): Pair<Int, Int> {
+            val rdRegex = Regex("([1-9][0-9]*)(?:[:dD])([1-9][0-9]*)")
+            val rdRegexRes = rdRegex.matchEntire(command)
+
+            if (rdRegexRes != null) {
+                val time = rdRegexRes.groupValues[1].toInt()
+                val max = rdRegexRes.groupValues[2].toInt()
+
+                if (time > 1000) {
+                    return Pair(-1, 0)
+                } else if (max > 1000) {
+                    return Pair(-2, 0)
+                }
+
+                return Pair(time, max)
+            } else {
+                val numRegex = Regex("([1-9][0-9]*)")
+                val numRegexRes = numRegex.matchEntire(command)
+                if (numRegexRes != null) {
+                    return Pair(0, numRegexRes.groupValues[1].toInt())
+                }
+            }
+            throw Exception("Command format error.")
+        }
+
+        private fun randomOnce(max: Int, time: Int): LongArray =
+                LongArray(time) {
+                    (1..max).random().toLong()
+                }
+    }
+}
