ff67d35aa0c01b0527f1d89b91411631177ba465
dazo66 <dazo66@dazo66.com>
2020-08-10 22:35:53 +0800

    完善框架

--- src/main/java/com/aye10032/Functions/funcutil/SimpleMsg.java
+++ src/main/java/com/aye10032/Functions/funcutil/SimpleMsg.java
@@ -1,13 +1,15 @@
 package com.aye10032.Functions.funcutil;
 
 import com.dazo66.command.interfaces.ICommand;
+import kotlin.Unit;
 import net.mamoe.mirai.message.FriendMessageEvent;
 import net.mamoe.mirai.message.GroupMessageEvent;
 import net.mamoe.mirai.message.MessageEvent;
 import net.mamoe.mirai.message.TempMessageEvent;
+import net.mamoe.mirai.message.data.*;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * CQMsg的包装对象 每个传入的消息都进行包装后交由模块执行
@@ -43,14 +45,35 @@ public class SimpleMsg implements ICommand {
     }
 
     private String getMsgFromEvent(MessageEvent event){
-        String s = event.getMessage().contentToString();
+        /*String s = event.getMessage().contentToString();
         Matcher matcher = MIRAI_PATTERN.matcher(s);
         String s1 = "";
         try {
             matcher.find();
             s1 = matcher.group();
         } catch (Exception e) {}
-        return s.substring(s1.length());
+        return s.substring(s1.length());*/
+        MessageChain chain = event.getMessage();
+        MessageChainBuilder builder = new MessageChainBuilder();
+        chain.forEachContent((Message m) -> {
+            if (m instanceof At) {
+                At at = (At) m;
+                try {
+                    Constructor c1 = At.class.getDeclaredConstructor(long.class, String.class);
+                    c1.setAccessible(true);
+                    at = (At) c1.newInstance(((At) m).getTarget(), ((At) m).getDisplay().replace(" ", ""));
+                } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException e) {
+                    e.printStackTrace();
+                }
+                builder.add(at);
+            }else if (m instanceof MessageSource) {
+                //ignore
+            } else {
+                builder.add(m);
+            }
+            return Unit.INSTANCE;
+        });
+        return builder.build().contentToString();
     }
 
     public long getFromGroup() {
@@ -119,6 +142,4 @@ public class SimpleMsg implements ICommand {
     public static SimpleMsg getTempMsg(String testMsg){
         return new SimpleMsg(995497677L, 2375985957L, testMsg, MsgType.GROUP_MSG);
     }
-
-    private static Pattern MIRAI_PATTERN = Pattern.compile("(\\[mirai:source[\\S|\\s]+])");
 }
