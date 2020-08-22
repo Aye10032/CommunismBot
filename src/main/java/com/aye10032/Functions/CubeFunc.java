43ce47683be33f18252732d3ee3fafad6c7c7601
“Aye10032 <yeyu6_505@126.com>
2020-08-23 00:17:33 +0800

    重写魔方功能

--- src/main/java/com/aye10032/Functions/CubeFunc.java
+++ src/main/java/com/aye10032/Functions/CubeFunc.java
@@ -1,8 +1,12 @@
 package com.aye10032.Functions;
 
 import com.aye10032.Functions.funcutil.BaseFunc;
+import com.aye10032.Functions.funcutil.FuncExceptionHandler;
 import com.aye10032.Functions.funcutil.SimpleMsg;
 import com.aye10032.Zibenbot;
+import com.dazo66.command.Commander;
+import com.dazo66.command.CommanderBuilder;
+import com.dazo66.command.interfaces.PieceCheck;
 
 import java.io.File;
 import java.util.Random;
@@ -16,26 +20,62 @@ public class CubeFunc extends BaseFunc {
     private int step = 20;
     private String cuberandom = "";
 
+    private Commander<SimpleMsg> commander;
+
 /*    public static void main(String[] args) {
         System.out.println(new AyeCube().getCuberandom());
     }*/
 
     public CubeFunc(Zibenbot zibenbot) {
         super(zibenbot);
-        Random random = new Random();
-        int num = random.nextInt(5);
-        step += num;
-        for (int i = 0; i < step; i++) {
-            String thisStep = nextStep(random) + statusarr[random.nextInt(statusarr.length)];
-            cubeStack.push(thisStep);
-        }
-
-        String temp = "";
-        while (!cubeStack.empty()) {
-            temp += cubeStack.pop() + " ";
-        }
-
-        setCuberandom(temp);
+        commander = new CommanderBuilder<SimpleMsg>()
+                .seteHandler(FuncExceptionHandler.INSTENCE)
+                .start()
+                .or(commandCheck(new String[]{".cube",".CUBE"}))
+                .run((cqmsg) -> {
+                    zibenbot.replyMsg(cqmsg, funcInfo());
+                })
+                .or(".3"::equals)
+                .run((cqmsg) -> {
+                    newCuberandom();
+                    zibenbot.replyMsg(cqmsg, getCuberandom());
+                })
+                .or(commandCheck(new String[]{".cfop",".CFOP"}))
+                .run((cqmsg) ->{
+                    zibenbot.replyMsg(cqmsg,
+                            zibenbot.getImg(new File(zibenbot.appDirectory + "\\image\\cube\\CFOP1.jpg"))
+                                    + zibenbot.getImg(new File(zibenbot.appDirectory + "\\image\\cube\\CFOP2.jpg"))
+                                    + zibenbot.getImg(new File(zibenbot.appDirectory + "\\image\\cube\\CFOP3.jpg"))
+                                    + zibenbot.getImg(new File(zibenbot.appDirectory + "\\image\\cube\\CFOP4.jpg")));
+                })
+                .next()
+                    .or(commandCheck(new String[]{"F2L","f2l"}))
+                    .run((cqmsg) ->{
+                        zibenbot.replyMsg(cqmsg, zibenbot.getImg(new File(zibenbot.appDirectory + "\\image\\cube\\CFOP2.jpg")));
+                    })
+                    .or(commandCheck(new String[]{"OLL","oll"}))
+                    .run((cqmsg) ->{
+                        zibenbot.replyMsg(cqmsg, zibenbot.getImg(new File(zibenbot.appDirectory + "\\image\\cube\\CFOP3.jpg")));
+                    })
+                    .or(commandCheck(new String[]{"PLL","pll"}))
+                    .run((cqmsg) ->{
+                        zibenbot.replyMsg(cqmsg, zibenbot.getImg(new File(zibenbot.appDirectory + "\\image\\cube\\CFOP4.jpg")));
+                    })
+                .pop()
+                .or(commandCheck(new String[]{".MEGA",".mega"}))
+                .run((cqmsg) ->{
+                    zibenbot.replyMsg(cqmsg,
+                            zibenbot.getImg(new File(zibenbot.appDirectory + "\\image\\cube\\Mega1.jpg"))
+                                    + zibenbot.getImg(new File(zibenbot.appDirectory + "\\image\\cube\\Mega2.jpg")));
+                })
+                .or(".22"::equals)
+                .run((cqmsg) ->{
+                    zibenbot.replyMsg(cqmsg,
+                            zibenbot.getImg(new File(zibenbot.appDirectory + "\\image\\cube\\2X201.png"))
+                                    + zibenbot.getImg(new File(zibenbot.appDirectory + "\\image\\cube\\2X202.png"))
+                                    + zibenbot.getImg(new File(zibenbot.appDirectory + "\\image\\cube\\2X203.png")));
+                })
+                .build();
     }
 
     private String nextStep(Random random) {
@@ -50,6 +90,36 @@ public class CubeFunc extends BaseFunc {
         }
 
         return temp;
+    }
+
+    private String funcInfo(){
+        StringBuilder infoBuilder = new StringBuilder();
+
+        infoBuilder
+                .append(".cube -----功能一览\n")
+                .append(".3 -----三阶打乱\n")
+                .append(".CFOP [F2L|OLL|PLL] -----CFOP公式\n")
+                .append(".22 -----二阶面先法公式\n")
+                .append(".MEGA -----五魔方公式\n");
+
+        return infoBuilder.toString();
+    }
+
+    private void newCuberandom() {
+        Random random = new Random();
+        int num = random.nextInt(5);
+        step += num;
+        for (int i = 0; i < step; i++) {
+            String thisStep = nextStep(random) + statusarr[random.nextInt(statusarr.length)];
+            cubeStack.push(thisStep);
+        }
+
+        String temp = "";
+        while (!cubeStack.empty()) {
+            temp += cubeStack.pop() + " ";
+        }
+
+        setCuberandom(temp);
     }
 
     public String getCuberandom() {
@@ -67,26 +137,22 @@ public class CubeFunc extends BaseFunc {
 
     @Override
     public void run(SimpleMsg CQmsg) {
-        if (CQmsg.getMsg().equals(".3")) {
-            zibenbot.replyMsg(CQmsg, getCuberandom());
-        }else if (CQmsg.getMsg().startsWith("CFOP")||CQmsg.getMsg().startsWith("cfop")) {
-            if (CQmsg.isTeamspealMsg()) {
-                zibenbot.replyMsg(CQmsg, "ts频道无法发图片，请从群聊或者私聊获取");
-                return;
+        commander.execute(CQmsg);
+    }
+
+    private PieceCheck commandCheck(String[] strings){
+        PieceCheck pieceCheck = new PieceCheck() {
+            @Override
+            public boolean check(String piece) {
+                for (String temp:strings){
+                    if (piece.equals(temp)){
+                        return true;
+                    }
+                }
+                return false;
             }
-            if (CQmsg.getMsg().contains("F2L") || CQmsg.getMsg().contains("f2l")) {
-                zibenbot.replyMsg(CQmsg, zibenbot.getImg(new File(zibenbot.appDirectory + "\\image\\CFOP2.jpg")));
-            } else if (CQmsg.getMsg().contains("OLL") || CQmsg.getMsg().contains("oll")) {
-                zibenbot.replyMsg(CQmsg, zibenbot.getImg(new File(zibenbot.appDirectory + "\\image\\CFOP3.jpg")));
-            } else if (CQmsg.getMsg().contains("PLL") || CQmsg.getMsg().contains("pll")) {
-                zibenbot.replyMsg(CQmsg, zibenbot.getImg(new File(zibenbot.appDirectory + "\\image\\CFOP4.jpg")));
-            } else {
-                zibenbot.replyMsg(CQmsg,
-                        zibenbot.getImg(new File(zibenbot.appDirectory + "\\image\\CFOP1.jpg"))
-                                + zibenbot.getImg(new File(zibenbot.appDirectory + "\\image\\CFOP2.jpg"))
-                                + zibenbot.getImg(new File(zibenbot.appDirectory + "\\image\\CFOP3.jpg"))
-                                + zibenbot.getImg(new File(zibenbot.appDirectory + "\\image\\CFOP4.jpg")));
-            }
-        }
+        };
+
+        return pieceCheck;
     }
 }
