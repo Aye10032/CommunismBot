606bd881b600410f3a97a324aac7a36793170c9a
“Aye10032 <yeyu6_505@126.com>
2020-08-23 17:29:48 +0800

    update

---
+++ data/phantomjs/example.bat
@@ -0,0 +1,3 @@
+"C:\Users\Administrator\Desktop\bot\酷Q Pro\data\app\org.meowy.cqp.jcq\data\app\com.aye10032.zibenbot\phantomjs\bin\phantomjs.exe" "C:\Users\Administrator\Desktop\bot\酷Q Pro\data\app\org.meowy.cqp.jcq\data\app\com.aye10032.zibenbot\phantomjs\screenshot.js" "http://ak.mooncell.wiki/w/推进之王" test.jpg 4000
+
+pause
