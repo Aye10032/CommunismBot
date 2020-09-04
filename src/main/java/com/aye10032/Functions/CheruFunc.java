f48dd6736e48040fa8fffe38f050f4c42cc55594
“Aye10032 <yeyu6_505@126.com>
2020-09-04 11:07:57 +0800

    add CheruFunc

---
+++ src/main/java/com/aye10032/Functions/CheruFunc.java
@@ -0,0 +1,41 @@
+package com.aye10032.Functions;
+
+import com.aye10032.Functions.funcutil.BaseFunc;
+import com.aye10032.Functions.funcutil.SimpleMsg;
+import com.aye10032.Utils.CheruUtil;
+import com.aye10032.Zibenbot;
+
+public class CheruFunc extends BaseFunc {
+
+    private CheruUtil cheruUtil;
+
+    public CheruFunc(Zibenbot zibenbot) {
+        super(zibenbot);
+    }
+
+    @Override
+    public void setUp() {
+        cheruUtil = new CheruUtil();
+    }
+
+    @Override
+    public void run(SimpleMsg CQmsg) {
+        if (CQmsg.getMsg().startsWith("切噜") && (CQmsg.getMsg().split(" ").length == 2)) {
+            String msg = "";
+            try {
+                msg = cheruUtil.toCheru(CQmsg.getMsg().split(" ")[1]);
+            } catch (Exception e) {
+                msg = e.toString();
+            }
+            zibenbot.replyMsg(CQmsg, msg);
+        } else if (CQmsg.getMsg().startsWith("切噜～")) {
+            String msg = "";
+            try {
+                msg = cheruUtil.toStr(msg);
+            } catch (Exception e) {
+                msg = e.toString();
+            }
+            zibenbot.replyMsg(CQmsg, msg);
+        }
+    }
+}
---
+++ src/main/java/com/aye10032/Utils/CheruUtil.java
@@ -0,0 +1,87 @@
+package com.aye10032.Utils;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CheruUtil {
+
+    char[] chars = "切卟叮咧哔唎啪啰啵嘭噜噼巴拉蹦铃".toCharArray();
+    private String split_text = "\\b";
+    private String word_text = "\\pP";
+    private Pattern pattern;
+
+    public CheruUtil() {
+        pattern = Pattern.compile(word_text);
+    }
+
+    public static void main(String[] args) {
+        CheruUtil cheruUtil = new CheruUtil();
+        System.out.println(cheruUtil.toStr("切噜～♪切叮噼叮蹦噜巴哔拉叮咧切切"));
+    }
+
+    public String toCheru(String msg) {
+        String[] msgs = msg.split(split_text);
+        StringBuilder stringBuilder = new StringBuilder();
+
+        stringBuilder.append("切噜～♪");
+
+        for (String word : msgs) {
+            Matcher matcher = pattern.matcher(word);
+            if (!matcher.find()) {
+                byte[] bs = Charset.forName("GB18030").encode(word).array();
+                int bit;
+                StringBuilder sb = new StringBuilder();
+                sb.append("切");
+                for (int i = 0; i < bs.length / 2; i++) {
+                    bit = bs[i] & 0x0f;
+                    sb.append(chars[bit]);
+
+                    bit = (bs[i] & 0x0f0) >> 4;
+                    sb.append(chars[bit]);
+                }
+                stringBuilder.append(sb.toString());
+            } else {
+                stringBuilder.append(word);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    public String toStr(String msg) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        msg = msg.substring(msg.indexOf("切噜～") + 4);
+        String[] msgs = msg.split(split_text);
+
+        for (String word : msgs) {
+            Matcher matcher = pattern.matcher(word);
+            if (!matcher.find()) {
+                if (word.startsWith("切")) {
+                    word = word.substring(1);
+                    char[] thischar = word.toCharArray();
+                    List<Byte> bytes = new ArrayList<>();
+                    for (int i = 0; i < thischar.length; i += 2) {
+                        int x1 = Arrays.binarySearch(chars, thischar[i]);
+                        int x2 = Arrays.binarySearch(chars, thischar[i + 1]);
+                        int x = x2 << 4 | x1;
+                        bytes.add((byte) x);
+                    }
+                    byte[] bt = new byte[bytes.size()];
+                    for (int i = 0; i < bt.length; i++) {
+                        bt[i] = bytes.get(i);
+                    }
+                    stringBuilder.append(Charset.forName("GB18030").decode(ByteBuffer.wrap(bt)));
+                }
+            } else {
+                stringBuilder.append(word);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+}
