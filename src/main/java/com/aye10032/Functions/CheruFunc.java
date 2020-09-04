b7a91398a2f45202b23023cc9f6c79316e071552
“Aye10032 <yeyu6_505@126.com>
2020-09-04 12:26:27 +0800

    fix bug

--- src/main/java/com/aye10032/Functions/CheruFunc.java
+++ src/main/java/com/aye10032/Functions/CheruFunc.java
@@ -29,7 +29,7 @@ public class CheruFunc extends BaseFunc {
             }
             zibenbot.replyMsg(CQmsg, msg);
         } else if (CQmsg.getMsg().startsWith("切噜～")) {
-            String msg = "";
+            String msg = CQmsg.getMsg();
             try {
                 msg = cheruUtil.toStr(msg);
             } catch (Exception e) {
