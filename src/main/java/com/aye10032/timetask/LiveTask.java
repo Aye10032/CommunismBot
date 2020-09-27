902e9b8f87adc03a8dc515d5d322f0c68e2bc728
“Aye10032 <yeyu6_505@126.com>
2020-09-27 14:29:34 +0800

    重写判定

--- src/main/java/com/aye10032/timetask/LiveTask.java
+++ src/main/java/com/aye10032/timetask/LiveTask.java
@@ -5,7 +5,6 @@ import com.aye10032.utils.ConfigLoader;
 import com.aye10032.utils.ImgUtils;
 import com.aye10032.utils.timeutil.Reciver;
 import com.aye10032.utils.timeutil.SubscribableBase;
-import com.aye10032.utils.video.LiveClass;
 import com.aye10032.utils.video.LiveInfo;
 
 import java.io.File;
@@ -17,12 +16,10 @@ import static com.aye10032.utils.timeutil.TimeUtils.MIN;
 public abstract class LiveTask extends SubscribableBase {
 
     private Zibenbot zibenbot;
-    private LiveClass liveClass;
 
     public LiveTask(Zibenbot zibenbot) {
         super(zibenbot);
         this.zibenbot = zibenbot;
-        this.liveClass = ConfigLoader.load(zibenbot.appDirectory + "/liveData.json", LiveClass.class);
     }
 
     @Override
@@ -37,23 +34,19 @@ public abstract class LiveTask extends SubscribableBase {
         StringBuilder msg_builder = new StringBuilder();
         Date now = new Date();
         if (recivers != null) {
-            liveClass = ConfigLoader.load(zibenbot.appDirectory + "/liveData.json", LiveClass.class);
             for (Reciver reciver : recivers) {
-                List<String> live_list = liveClass.getList(reciver.getSender().getFromGroup());
-                for (String live : live_list) {
-                    LiveInfo liveInfo = new LiveInfo(live);
-                    if (liveInfo.HasLive()&&liveInfo.Is_living()) {
-                        long l = now.getTime() - liveInfo.getLive_date().getTime();
-                        long min = ((l / (60 * 1000)));
-                        if (min<5) {
-                            ImgUtils.downloadImg(liveInfo.getLive_background_url(), live, zibenbot.appDirectory);
+                LiveInfo liveInfo = new LiveInfo(args[0]);
+                if (liveInfo.HasLive() && liveInfo.Is_living()) {
+                    long l = now.getTime() - liveInfo.getLive_date().getTime();
+                    long min = ((l / (60 * 1000)));
+                    if (min < 5) {
+                        ImgUtils.downloadImg(liveInfo.getLive_background_url(), args[0], zibenbot.appDirectory);
 
-                            msg_builder.append(liveInfo.getNickName(liveInfo.getUid())).append("正在直播：")
-                                    .append(liveInfo.getLive_title())
-                                    .append(zibenbot.getImg(new File(zibenbot.appDirectory + "\\image\\" + live + ".jpg")))
-                                    .append("\n").append(liveInfo.getLive_url());
-                            zibenbot.replyMsg(reciver.getSender(), msg_builder.toString());
-                        }
+                        msg_builder.append(liveInfo.getNickName(liveInfo.getUid())).append("正在直播：")
+                                .append(liveInfo.getLive_title())
+                                .append(zibenbot.getImg(new File(zibenbot.appDirectory + "\\image\\" + args[0] + ".jpg")))
+                                .append("\n").append(liveInfo.getLive_url());
+                        zibenbot.replyMsg(reciver.getSender(), msg_builder.toString());
                     }
                 }
             }
