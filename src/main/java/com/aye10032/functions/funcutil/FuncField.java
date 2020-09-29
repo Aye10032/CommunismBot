db52225e32fffea66674bb53d1a4c24e24bc698b
dazo66 <dazo66@dazo66.com>
2020-09-29 21:28:37 +0800

    fix bug

--- src/main/java/com/aye10032/functions/funcutil/FuncField.java
+++ src/main/java/com/aye10032/functions/funcutil/FuncField.java
@@ -1,10 +1,16 @@
 package com.aye10032.functions.funcutil;
 
+import java.lang.annotation.*;
+
 /**
  * 方法字段注解 只可用于Zibenbot这个主类中
  * 用这个注解的字段会在{@link FuncLoader}加载对象的时候进行写入
  *
  * @author Dazo66
  */
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD})
 public @interface FuncField {
 }
