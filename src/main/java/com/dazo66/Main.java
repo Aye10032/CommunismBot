fa681dfeb51f684dcd9cc4a2d72489d7e231ea1f
dazo6 <dazo66@126.com>
2022-06-02 21:39:47 +0800

    修复依赖问题

--- src/main/java/com/dazo66/Main.java
+++ src/main/java/com/dazo66/Main.java
@@ -1,11 +1,14 @@
 package com.dazo66;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
 
 @SpringBootApplication(scanBasePackages = {"com.dazo66", "com.aye10032"})
+@MapperScans({@MapperScan("com.dazo66"), @MapperScan("com.aye10032")})
 public class Main {
 
     public static void main(String[] args) throws IOException {
