23327ab0a0103a9bd20fbaf9dd0a96c664b17b90
Aye10032 <aye10032@gmail.com>
2022-07-17 18:26:22 +0800

    整合关键词功能

--- src/main/java/com/aye10032/functions/KeyWordFunc.java
+++ src/main/java/com/aye10032/functions/KeyWordFunc.java
@@ -1,9 +1,9 @@
 package com.aye10032.functions;
 
+import com.aye10032.Zibenbot;
 import com.aye10032.functions.funcutil.BaseFunc;
 import com.aye10032.functions.funcutil.FuncExceptionHandler;
 import com.aye10032.functions.funcutil.SimpleMsg;
-import com.aye10032.Zibenbot;
 import com.dazo66.command.Commander;
 import com.dazo66.command.CommanderBuilder;
 
@@ -13,27 +13,33 @@ import java.util.Random;
 /**
  * @author Dazo66
  */
-public class liantongFunc extends BaseFunc {
+public class KeyWordFunc extends BaseFunc {
 
     private Random random;
     private Commander<SimpleMsg> commander;
 
-    public liantongFunc(Zibenbot zibenbot) {
+    public KeyWordFunc(Zibenbot zibenbot) {
         super(zibenbot);
         random = new Random(System.currentTimeMillis());
         commander = new CommanderBuilder<SimpleMsg>()
                 .seteHandler(FuncExceptionHandler.INSTENCE)
                 .start()
+                .or("nmsl"::equals)
+                .run((msg) -> {
+                    zibenbot.replyMsg(msg, zibenbot.at(msg.getFromClient())
+                            + " " + zibenbot.getImg(new File(appDirectory + "\\image\\dragon.jpg"))
+                            + " 疯牛满地跑，难免输了");
+                })
                 .or("炼铜"::contains)
-                .run((msg)->{
-                    if (random.nextDouble() < 0.2d) {
-                        zibenbot.replyMsg(msg, zibenbot.getImg(new File(zibenbot.appDirectory + "\\image\\liantong.jpg")));
+                .run((msg) -> {
+                    if (random.nextDouble() < 0.4d) {
+                        zibenbot.replyMsg(msg, zibenbot.getImg(new File(appDirectory + "\\image\\liantong.jpg")));
                     }
                 })
                 .or("疯狂星期四"::contains)
-                .run((msg)->{
-                    if (random.nextDouble() < 0.4d){
-                        zibenbot.replyMsg(msg,"朋友，我没有50\n" +
+                .run((msg) -> {
+                    if (random.nextDouble() < 0.8d) {
+                        zibenbot.replyMsg(msg, "朋友，我没有50\n" +
                                 "别再转发疯狂星期四了\n" +
                                 "我建议你去吃华莱士\n" +
                                 "50可以买TM七八个汉堡\n" +
