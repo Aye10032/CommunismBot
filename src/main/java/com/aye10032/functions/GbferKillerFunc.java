4947ee1073e6ff5edf182bb00cde7977e014abc4
Aye10032 <aye10032@gmail.com>
2022-07-20 19:33:16 +0800

    修复图片路径

--- src/main/java/com/aye10032/functions/GbferKillerFunc.java
+++ src/main/java/com/aye10032/functions/GbferKillerFunc.java
@@ -19,7 +19,7 @@ public class GbferKillerFunc extends BaseFunc {
     public void run(SimpleMsg simpleMsg) {
         if (simpleMsg.getMsg().contains("granbluefantasy")) {
             zibenbot.muteMember(simpleMsg.getFromGroup(), simpleMsg.getFromClient(), 114);
-            zibenbot.replyMsgWithQuote(simpleMsg, "空骑士爪巴" + zibenbot.getImg(appDirectory + "\\gbfKiller.jpg"));
+            zibenbot.replyMsgWithQuote(simpleMsg, "空骑士爪巴" + zibenbot.getImg(appDirectory + "/gbfKiller.jpg"));
         }
     }
 }
