1887b8b4590e933c89261d24cc53f72ce39179e2
Aye10032 <aye10032@gmail.com>
2022-10-01 20:46:48 +0800

    测试用

--- src/main/java/com/aye10032/functions/VideoFunction.java
+++ src/main/java/com/aye10032/functions/VideoFunction.java
@@ -4,6 +4,7 @@ import com.aye10032.Zibenbot;
 import com.aye10032.functions.funcutil.BaseFunc;
 import com.aye10032.functions.funcutil.FuncExceptionHandler;
 import com.aye10032.functions.funcutil.SimpleMsg;
+import com.dazo66.command.CommandPiece;
 import com.dazo66.command.Commander;
 import com.dazo66.command.CommanderBuilder;
 import org.springframework.stereotype.Service;
@@ -19,8 +20,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class VideoFunction extends BaseFunc {
 
+    private Commander<SimpleMsg> commander;
+
     public VideoFunction(Zibenbot zibenbot) {
         super(zibenbot);
+        commander = new CommanderBuilder<SimpleMsg>()
+                .seteHandler(FuncExceptionHandler.INSTENCE)
+                .start()
+                .or(this::isAudio)
+                .run((msg)->{
+                    if (msg.isPrivateMsg()&&msg.getFromClient() == 2375985957L){
+                        zibenbot.replyMsg(msg, msg.getMsg());
+                    }
+                })
+                .or(s->true)
+                .run((msg)->{
+                    if (msg.isPrivateMsg()&&msg.getFromClient() == 2375985957L){
+                        zibenbot.replyMsg(msg, msg.getMsg());
+                    }
+                })
+                .build();
     }
 
     @Override
@@ -30,8 +49,10 @@ public class VideoFunction extends BaseFunc {
 
     @Override
     public void run(SimpleMsg simpleMsg) {
-        if (simpleMsg.getFromClient() == 2375985957L){
-            zibenbot.replyMsg(simpleMsg, simpleMsg.getMsg());
-        }
+        commander.execute(simpleMsg);
+    }
+
+    private boolean isAudio(String msg){
+        return msg.startsWith("[mirai:audio:");
     }
 }
