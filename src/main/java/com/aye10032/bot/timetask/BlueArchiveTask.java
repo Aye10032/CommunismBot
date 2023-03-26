355c838fecfd6ebc9c8961bdecd7f9a75a0e9613
Aye10032 <aye10032@gmail.com>
2023-03-26 12:41:21 +0800

    BA动态订阅

--- src/main/java/com/aye10032/bot/timetask/BlueArchiveTask.java
+++ src/main/java/com/aye10032/bot/timetask/BlueArchiveTask.java
@@ -1,10 +1,16 @@
 package com.aye10032.bot.timetask;
 
+import com.aye10032.foundation.utils.ImgUtils;
 import com.aye10032.foundation.utils.timeutil.Reciver;
 import com.aye10032.foundation.utils.timeutil.SubscribableBase;
+import com.aye10032.foundation.utils.video.Dynamic;
+import com.aye10032.foundation.utils.video.DynamicInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -26,11 +32,32 @@ public class BlueArchiveTask extends SubscribableBase {
 
     @Override
     public void run(List<Reciver> recivers, String[] args) {
+        StringBuilder builder = new StringBuilder();
+        Date now = new Date();
+        log.info("开始获取资讯站动态");
+        DynamicInfo dynamicInfo = new DynamicInfo("3750792");
+        List<Dynamic> dynamics = dynamicInfo.getDynamics();
+        Collections.reverse(dynamics);
+        for (Dynamic dynamic : dynamics) {
+            long l = now.getTime() - dynamic.getPub_time().getTime();
+            long min = ((l / (60 * 1000)));
+            if (dynamic.getText().contains("日服资讯") && l < 5 * 60 * 1000) {
+                log.info("获取到一条新动态:" + dynamic.getDynamic_id());
+                builder.append(dynamic.getText());
 
+                int img_count = dynamic.getImg_url_list().size();
+                for (int i = 0; i < img_count; i++) {
+                    ImgUtils.downloadImg(dynamic.getImg_url_list().get(i), "ba" + i, getBot().appDirectory);
+                    builder.append(getBot().getImg(new File(getBot().appDirectory + "/image/" + "ba" + i + ".jpg")));
+                }
+                builder.append(dynamic.getDynamic_url());
+                replyAll(recivers, builder.toString());
+            }
+        }
     }
 
     @Override
     public String getCron() {
-        return null;
+        return "0 0/5 * * * ? ";
     }
 }
