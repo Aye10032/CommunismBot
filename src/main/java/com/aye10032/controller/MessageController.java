7d6ad2ad4ba5ed848fae1be7820926490b3c4ec8
Aye10032 <yeyu6_505@126.com>
2023-06-03 10:18:52 +0800

    测试GitHub监听

--- src/main/java/com/aye10032/controller/MessageController.java
+++ src/main/java/com/aye10032/controller/MessageController.java
@@ -3,6 +3,8 @@ package com.aye10032.controller;
 import com.aye10032.bot.Zibenbot;
 import com.aye10032.foundation.entity.dto.Result;
 import com.aye10032.service.FFXIVService;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -57,6 +59,42 @@ public class MessageController {
         String filename = formattedDateTime + "-data.json";
         File file = new File(filename);
         FileUtils.writeStringToFile(file, data, StandardCharsets.UTF_8);
+    }
+
+    @PostMapping("/githubCreat")
+    public void creatEvent(@RequestBody String data_string) throws IOException {
+        String repo = "";
+        String pusher = "";
+        String time = "";
+        JsonObject data = JsonParser.parseString(data_string).getAsJsonObject();
+        repo = data.get("repository").getAsJsonObject().get("url").getAsString();
+        pusher = data.get("pusher").getAsJsonObject().get("name").getAsString();
+        time = data.get("head_commit").getAsJsonObject().get("timestamp").getAsString();
+
+        String msg = pusher + "刚刚创建了一个新的仓库：\n" + repo + "\n----------------" + time;
+
+        zibenbot.toGroupMsg(1044102726L, msg);
+    }
+
+    @PostMapping("/githubPush")
+    public void pushEvent(@RequestBody String data_string) throws IOException {
+        String branch = "";
+        String repo = "";
+        String pusher = "";
+        String commit = "";
+        String time = "";
+        JsonObject data = JsonParser.parseString(data_string).getAsJsonObject();
+        branch = data.get("ref").getAsString().split("/")[2];
+        repo = data.get("repository").getAsJsonObject().get("url").getAsString();
+        pusher = data.get("pusher").getAsJsonObject().get("name").getAsString();
+        commit = data.get("head_commit").getAsJsonObject().get("message").getAsString();
+        time = data.get("head_commit").getAsJsonObject().get("timestamp").getAsString();
+
+        String msg = "来自 " + repo + " 的消息：\n"
+                + pusher + "刚刚向" + branch + "分支提交了一个更新，内容为：\n"
+                + commit + "\n----------------" + time;
+
+        zibenbot.toGroupMsg(1044102726L, msg);
     }
 
 }
