fb8c1583630af5c6692936e374facdb0d85bea82
dazo66 <dazo66@126.com>
2023-08-14 00:39:52 +0800

    调整自增值

--- src/main/java/com/aye10032/foundation/entity/base/dream/Dream.java
+++ src/main/java/com/aye10032/foundation/entity/base/dream/Dream.java
@@ -1,6 +1,7 @@
 package com.aye10032.foundation.entity.base.dream;
 
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -13,6 +14,7 @@ import java.util.Date;
  * @date 2023-08-10
  */
 @Data
+@TableName("dream")
 public class Dream {
     /**
      * 
