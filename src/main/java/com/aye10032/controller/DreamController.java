8d530db19378bd90ffe8ac077af0b53e3e1a8247
Aye10032 <aye10032@gmail.com>
2023-08-17 10:23:59 +0800

    适配新版注释结构

--- src/main/java/com/aye10032/controller/DreamController.java
+++ src/main/java/com/aye10032/controller/DreamController.java
@@ -6,6 +6,7 @@ import com.aye10032.service.DreamService;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
  * @author dazo66(sundazhong.sdz)
  * @date 2023/8/13 23:34
  **/
-@RestController("dream")
+
+@RestController()
+@RequestMapping("dream")
 public class DreamController {
 
     @Autowired
