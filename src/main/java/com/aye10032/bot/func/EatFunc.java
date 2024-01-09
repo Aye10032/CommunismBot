392e075faaeb4b534c318b0c89f15144010df6b6
Aye10032 <aye10032@gmail.com>
2024-01-09 16:16:44 +0800

    随机封装

--- src/main/java/com/aye10032/bot/func/EatFunc.java
+++ src/main/java/com/aye10032/bot/func/EatFunc.java
@@ -14,6 +14,9 @@ import com.aye10032.foundation.utils.command.CommanderBuilder;
 import com.aye10032.foundation.utils.food.FoodClaass;
 import org.springframework.stereotype.Service;
 
+import static com.aye10032.foundation.utils.RandomUtil.getRandom;
+import static com.aye10032.foundation.utils.RandomUtil.getRandomWithSSR;
+
 /**
  * @author Aye10032
  */
@@ -21,7 +24,6 @@ import org.springframework.stereotype.Service;
 public class EatFunc extends BaseFunc {
 
     private FoodUtil foodUtil;
-    private RandomUtil randomUtil;
     private EatData eatData;
     FoodClaass foodClaass = ConfigLoader.load(zibenbot.appDirectory + "/foodData.json", FoodClaass.class);
     private Commander<SimpleMsg> commander;
@@ -35,36 +37,36 @@ public class EatFunc extends BaseFunc {
                 .run((cqmsg) -> {
                     String[] total_list = ArrayUtils.concatAll(
                             eatData.getTongyi(), eatData.getKangshifu(), eatData.getHeweidao(), eatData.getHeweidao());
-                    zibenbot.replyMsg(cqmsg, randomUtil.getRandom(total_list));
+                    zibenbot.replyMsg(cqmsg, getRandom(total_list));
                 })
                 .next()
                 .or("统一"::equals)
                 .run((cqmsg) -> {
-                    zibenbot.replyMsg(cqmsg, randomUtil.getRandom(eatData.getTongyi()) + "面");
+                    zibenbot.replyMsg(cqmsg, getRandom(eatData.getTongyi()) + "面");
                 })
                 .or("康师傅"::equals)
                 .run((cqmsg) -> {
-                    zibenbot.replyMsg(cqmsg, randomUtil.getRandom(eatData.getKangshifu()) + "面");
+                    zibenbot.replyMsg(cqmsg, getRandom(eatData.getKangshifu()) + "面");
                 })
                 .or("合味道"::equals)
                 .run((cqmsg) -> {
-                    zibenbot.replyMsg(cqmsg, randomUtil.getRandom(eatData.getHeweidao()) + "面");
+                    zibenbot.replyMsg(cqmsg, getRandom(eatData.getHeweidao()) + "面");
                 })
                 .or("汤达人"::equals)
                 .run((cqmsg) -> {
-                    zibenbot.replyMsg(cqmsg, randomUtil.getRandom(eatData.getTangdaren()) + "面");
+                    zibenbot.replyMsg(cqmsg, getRandom(eatData.getTangdaren()) + "面");
                 })
                 .pop()
                 .or(".一食堂"::equals)
                 .run((cqmsg) -> {
                     if (cqmsg.getFromGroup() == 792666782L || cqmsg.getFromGroup() == 295904863L) {
-                        zibenbot.replyMsg(cqmsg, randomUtil.getRandomWithSSR(
+                        zibenbot.replyMsg(cqmsg, getRandomWithSSR(
                                 eatData.getCanteen1(), eatData.getCanteen1sr(), eatData.getCanteen1ssr(), 30, 100)[0]);
                     }
                 })
                 .or(".晚饭"::equals)
                 .run((cqmsg) -> {
-                    zibenbot.replyMsg(cqmsg, randomUtil.getRandomWithSSR(
+                    zibenbot.replyMsg(cqmsg, getRandomWithSSR(
                             eatData.getMainlist(), eatData.getSrList(), eatData.getSsrList(), 30, 100)[0]);
                 })
                 .build();
@@ -73,7 +75,6 @@ public class EatFunc extends BaseFunc {
     @Override
     public void setUp() {
         this.foodUtil = new FoodUtil();
-        this.randomUtil = new RandomUtil();
         this.eatData = new EatData();
     }
 
