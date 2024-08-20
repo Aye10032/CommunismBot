0992f7b164d57a56edc26a6c5f5babccc95fd058
dazo <sundz@91160.com>
2024-08-20 14:23:00 +0800

    修复ban人

--- src/main/java/com/aye10032/bot/func/BanFunc.java
+++ src/main/java/com/aye10032/bot/func/BanFunc.java
@@ -75,6 +75,8 @@ public class BanFunc extends BaseFunc {
                         e.printStackTrace();
                     }
                 })
+                .pop()
+                .pop()
                 .or(".击杀榜"::equals)
                 .run((msg) -> {
                     List<KillRecord> records = killRecordService.selectKillRecordByGroup(msg.getFromGroup(), KILLER);
